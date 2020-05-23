74d977f2f9504225d2cc85c3b92a043789e785b9
Sidharth Nambiar <snambiar01@gmail.com>
2020-05-22 23:28:58 -0400

    completed pigify

--- basics/00-vowel-count/vowel-count.js
+++ basics/00-vowel-count/vowel-count.js
@@ -1,1 +1,35 @@
 /* eslint-disable no-unused-vars */
+
+
+function vowelsCount(str) {
+
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    let lowerCaseStr = str.toLowerCase();
+    let chars = lowerCaseStr.split('');
+    let result = { a: 0, e: 0, i: 0, o: 0, u: 0 };
+
+    let totalVowels = chars.reduce((total, char) => {
+        if (vowels.includes(char)) {
+            total += 1;
+
+            if (result.hasOwnProperty(char)) {
+                result[char] += 1;
+            }
+            else {
+                result[char] = 1;
+            }
+
+        }
+
+        return total;
+
+    }, 0)
+
+    result["total"] = totalVowels;
+
+    return result;
+
+
+
+}
--- basics/01-pig-latin/pig-latin.js
+++ basics/01-pig-latin/pig-latin.js
@@ -1,1 +1,46 @@
 /* eslint-disable no-unused-vars */
+
+function pigify(words) {
+
+    let vowels = ['a', 'e', 'i', 'o'];
+    let pigifyChars = 'ay'
+    let singlePhenomes = ['sch', 'qu'];
+
+    let splitWords = words.split(' ');
+
+    let result = splitWords.map((word) => {
+
+        if (vowels.includes(word[0])) {
+            return word + pigifyChars;
+        }
+        else if (word.startsWith('qu')) {
+            return word.slice(2, word.length) + 'qu' + pigifyChars;
+        }
+        else if (word.startsWith('sch')) {
+            return word.slice(3, word.length) + 'sch' + pigifyChars;
+        }
+        else {
+            let chars = word.split('');
+            let suffix = '';
+            let index = null;
+
+            for (let i = 0; i < chars.length; i++) {
+                if(vowels.includes(chars[i])){
+                    break;
+                }
+
+                suffix += chars[i];
+                index = i;
+
+            }
+
+            return word.slice(index+1, word.length) + suffix + pigifyChars;
+
+
+        }
+
+    })
+    result = result.join(' ');
+    return result;
+}
+
