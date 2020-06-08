11514c392f3ea80195164fa5b58f3aa7ff0bd375
Sidharth Nambiar <snambiar01@gmail.com>
2020-06-07 23:48:47 -0400

    complete

--- this-keyword-context/00-call-all/call-all.js
+++ this-keyword-context/00-call-all/call-all.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+
+
+function callAll(obj, arrOfFunctions) {
+
+    let result = arrOfFunctions.map((callback) => {
+
+        return callback.call(obj)
+    })
+
+    return result;
+
+}
--- this-keyword-context/01-do-not-include/do-not-include.js
+++ this-keyword-context/01-do-not-include/do-not-include.js
@@ -1,1 +1,19 @@
 /* eslint-disable no-unused-vars, no-extend-native */
+
+
+Array.prototype.doNotInclude = function (arr) {
+
+    if(typeof arr === 'number'){
+       arr = [arr]
+    }
+
+    let result = this.filter((element, index) => {
+        
+        return !arr.includes(index)
+    
+
+        
+    })
+
+    return result;
+}
